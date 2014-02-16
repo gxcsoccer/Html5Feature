7329d07d9a3b8ce7745ced754d244fe22e0c1b32
高晓晨 <gaoxiaochen@gaoxiaochendeMacBook-Pro.local>
2014-02-16 23:42:36 +0800

    update

--- public/offline-iframe/js/offliner.js
+++ public/offline-iframe/js/offliner.js
@@ -1,9 +1,9 @@
 var offliner = (function() {
-	return {
-		addCachingIframe: function() {
-			$( document.body ).prepend(
-				'<iframe src="/offline-iframe/offline.html" style="position:absolute;top:-999em;visibility:hidden"></iframe>'
-			);
-		}
-	};
+  return {
+    addCachingIframe: function() {
+      $(document.body).prepend(
+        '<iframe src="offline.html" style="position:absolute;top:-999em;visibility:hidden"></iframe>'
+      );
+    }
+  };
 })();
