a27a572c7e331987586d8f3896fab15943f72cfb
gxcsoccer <gxcsoccer@126.com>
2014-02-17 09:23:57 +0800

    update

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-  app = express(),
-  path = require('path'),
-  fs = require('fs');
-
-app.use(express.logger());
-app.use(express.static(path.join(__dirname, '/public')));
-app.listen(8000);
