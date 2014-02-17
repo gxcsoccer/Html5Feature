d8cbb6fa1b5739b914a81e92140f8268325fbb6f
gxcsoccer <gxcsoccer@126.com>
2014-02-17 13:25:13 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express'),
   app = express(),
   path = require('path'),
-  fs = require('fs');
+  fs = require('fs'),
+  maxAge = 10368000;
 
+app.use(express.compress({
+  threshold: false
+}));
 app.use(express.logger());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(__dirname + '/public', {
+  maxAge: maxAge
+}));
+// app.use(express.static(__dirname + '/public'));
+
 app.listen(8000);
