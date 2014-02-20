fd194bd774f39c000a0ceef88f81e9ca39132a28
gxcsoccer <gxcsoccer@126.com>
2014-02-20 13:25:52 +0800

    update

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ var express = require('express'),
   app = express(),
   server = http.createServer(app),
   io = require('socket.io').listen(server),
-  maxAge = 10368000;
+  maxAge = 600000;
 
-// app.use(express.compress({
-//   threshold: false
-// }));
+app.use(express.compress({
+  threshold: false
+}));
 app.use(express.logger());
-// app.use(express.static(__dirname + '/public', {
-//   maxAge: maxAge
-// }));
-app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  res.setHeader('Vary', 'Accept-Encoding');
+  next();
+});
+app.use(express.static(__dirname + '/public', {
+  maxAge: maxAge
+}));
+// app.use(express.static(__dirname + '/public'));
 
 server.listen(8000);
 
@@ -25,9 +29,6 @@ io.sockets.on('connection', function(socket) {
     id: socket.id,
     count: count
   });
-  // socket.broadcast.emit('online', {
-  //   id: socket.id
-  // });
 
   socket.on('disconnect', function() {
     count--;
@@ -35,5 +36,9 @@ io.sockets.on('connection', function(socket) {
       id: socket.id,
       count: count
     });
+  });
+
+  socket.on('message', function(msg) {
+    socket.broadcast.emit('message', msg);
   });
 });
